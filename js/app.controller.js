593f14b9b0db0ac38a5b8022101f36c549304a7e
Avishai Cohen <avishaic2016@gmail.com>
2023-08-30 19:08:21 +0300

    adding render location and get User Pos

--- js/app.controller.js
+++ js/app.controller.js
@@ -6,6 +6,8 @@ window.onAddMarker = onAddMarker
 window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
+window.onDelete = onDelete
+window.onGo = onGo
 
 function onInit() {
     mapService.initMap()
@@ -23,22 +25,22 @@ function getPosition() {
     })
 }
 
-function onAddMarker() {
+function onAddMarker(lat = 32.0749831, lng = 34.9120554) {
     console.log('Adding a marker')
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+    mapService.addMarker({ lat, lng })
 }
 
 function onGetLocs() {
     locService.getLocs()
-        .then(locs => {
-            console.log('Locations:', locs)
-            document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
-        })
+        .then(locs => renderLocs(locs))
+    // document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
 }
 
 function onGetUserPos() {
     getPosition()
         .then(pos => {
+            onPanTo(pos.coords.latitude, pos.coords.longitude)
+            onAddMarker(pos.coords.latitude, pos.coords.longitude)
             console.log('User position is:', pos.coords)
             document.querySelector('.user-pos').innerText =
                 `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
@@ -47,7 +49,34 @@ function onGetUserPos() {
             console.log('err!!!', err)
         })
 }
-function onPanTo() {
+function onPanTo(lat = 35.6895, lng = 139.6917) {
     console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+    mapService.panTo(lat, lng)
+}
+
+function renderLocs(locs) {
+    console.log(locs)
+    const strHTMLs = locs.map(loc => {
+        // const id = makeId() // כדי למחוק צריך id ולא this
+        return `
+    <li>
+        <span>${loc.name}</span>
+        <button class="btn-delete" onclick="onDelete(this)">Delete</button>
+        <button class="btn-go" onclick="onGo(this)">Go</button>
+    </li>
+    `
+    })
+    document.querySelector('.locations').innerHTML = strHTMLs.join('')
+}
+
+function onDelete(elDelete) {
+    console.log(elDelete)
+    console.log('Delete')
+    // delete()
+}
+
+function onGo(elGo) {
+    console.log(elGo)
+    console.log('Go')
+    // go()
 }
