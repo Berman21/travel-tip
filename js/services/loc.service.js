bca15fd00280061099a12ae7932d8668be08e333
Yosef Berman <yosefberman21@gmail.com>
2023-08-30 21:58:07 +0300

    getGeo check

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -26,9 +26,14 @@ function getLocs() {
 }
 
 function getGeo(value) {
+    let prm = storageService.query(KEY).then(locs => locs.find(loc => loc.name === value))
+    if(prm) return prm
+
     console.log('ajax for geocode');
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${value}&key=${api.GOOGLE_MAP_KEY}`)
-        .then(res => res.data.results[0].geometry.location)  //{lng:34.4546 ; lat:23.4322}
+        .then(res => {
+            console.log('from ajax',res.data.results[0].geometry.location);
+            return res.data.results[0].geometry.location})  //{lng:34.4546 ; lat:23.4322}
         
 }   
 
@@ -49,5 +54,6 @@ function deleteLoc(id) {
 }
 
 function getLoc(id){
+    console.log('from cache');
     return storageService.get(KEY,id)
 }
